51d9cc4ddeeaee59d726625d24ce15a892477f11
uoslau <221911080@stis.ac.id>
2024-01-13 01:57:30 +0700

    laravel-17

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\DashboardPostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,14 +21,12 @@ use App\Http\Controllers\RegisterController;
 Route::get('/', function () {
     return view('home', [
         "title" =>  "Home",
-        "active" => "home"
     ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
         "title" =>  "About",
-        "active" => "about",
         "name"  =>  "Nelson Samosir",
         "email" =>  "nelsonsamosir36@gmail.com",
         "image" =>  "nelson.jpg"
@@ -43,7 +41,6 @@ Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 Route::get('/categories', function(){
     return view('categories', [
         'title' => 'Post Categories',
-        'active' => 'categories',
         'categories' => Category::all()
     ]);
 });
@@ -56,4 +53,8 @@ Route::post('/logout', [LoginController::class, 'logout']);
 Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store']);
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
+Route::get('/dashboard', function(){
+    return view('dashboard.index');
+})->middleware('auth');
+
+Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
